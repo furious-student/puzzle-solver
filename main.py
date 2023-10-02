ea384bfeb4be6750dbd7d5d9d3f0994d620583f1
Martin Sivak <xsivakm1@stuba.sk>
2023-10-02 10:00:56 +0200

    Add functionality to manipulate space_matrix

--- main.py
+++ main.py
@@ -1,5 +1,19 @@
+from state_node import StateNode
+
+
 def main():
-    print("Hello")
+    space_matrix = [
+        [1, 2, 3, 11],
+        [4, 0, 5, 10],
+        [6, 7, 8, 14]
+    ]
+    root = StateNode(space_matrix=space_matrix)
+    root.print_space_matrix()
+    print()
+    root.create_right_child()
+    root.create_right_child()
+    root.create_right_child()
+    root.print_space_matrix()
 
 
 if __name__ == '__main__':
--- state_node.py
+++ state_node.py
@@ -1,37 +1,98 @@
-from typing import List, Optional, Dict
+from typing import List, Optional, Dict, Tuple
 
 
 class StateNode:
-    __space: List[List[int]]
+    __space_matrix: List[List[int]]
     __parent: Optional["StateNode"]
     __children: Dict[str, Optional["StateNode"]]
     __path_operation: str
 
-    def __init__(self, space: List[List[int]]):
-        self.__space = space
-        self.__parent = None
+    def __init__(self, space_matrix: List[List[int]], parent: Optional["StateNode"] = None, path_operation: str = None):
+        self.__space_matrix = space_matrix
+        self.__parent = parent
         self.__children = {
             "left": None,
             "right": None,
             "up": None,
             "down": None
         }
-        self.__path_operation: None
+        self.__path_operation: path_operation
 
-    def move_right(self):
-        pass
+    def get_space_matrix(self) -> List[List[int]]:
+        return self.__space_matrix
 
-    def move_left(self):
-        pass
+    def print_space_matrix(self) -> None:
+        for row in self.__space_matrix:
+            print(row)
 
-    def move_down(self):
-        pass
+    def create_right_child(self) -> None:
+        empty_slot = self.__find_element(space_matrix=self.__space_matrix, element=0)
+        empty_x, empty_y = empty_slot
+        if empty_y > 0:
+            elm_to_move = [empty_x, empty_y - 1]
+            self.__space_matrix = self.__swap(space_matrix=self.__space_matrix,
+                                              el1_pos=empty_slot,
+                                              el2_pos=elm_to_move)
 
-    def move_up(self):
-        pass
+    def create_left_child(self) -> None:
+        empty_slot = self.__find_element(space_matrix=self.__space_matrix, element=0)
+        empty_x, empty_y = empty_slot
+        if empty_y < len(self.__space_matrix[0]) - 1:
+            elm_to_move = [empty_x, empty_y + 1]
+            self.__space_matrix = self.__swap(space_matrix=self.__space_matrix,
+                                              el1_pos=empty_slot,
+                                              el2_pos=elm_to_move)
 
-    def compute_h_1(self):
-        pass  # Number of pieces not on the right place
+    def create_down_child(self) -> None:
+        empty_slot = self.__find_element(space_matrix=self.__space_matrix, element=0)
+        empty_x, empty_y = empty_slot
+        if empty_x > 0:
+            elm_to_move = [empty_x - 1, empty_y]
+            self.__space_matrix = self.__swap(space_matrix=self.__space_matrix,
+                                              el1_pos=empty_slot,
+                                              el2_pos=elm_to_move)
 
-    def compute_h_2(self):
-        pass  # Sum of the distances of individual pieces from their final location
+    def create_up_child(self) -> None:
+        empty_slot = self.__find_element(space_matrix=self.__space_matrix, element=0)
+        empty_x, empty_y = empty_slot
+        if empty_x < len(self.__space_matrix) - 1:
+            elm_to_move = [empty_x + 1, empty_y]
+            self.__space_matrix = self.__swap(space_matrix=self.__space_matrix,
+                                              el1_pos=empty_slot,
+                                              el2_pos=elm_to_move)
+
+    def __create_child(self, space_matrix: List[List[int]], path_operation: str = None) -> None:
+        ops = ["left, right, up, down"]
+        if path_operation not in ops:
+            raise Exception(f"Argument \"path_operation\" {path_operation} not from {ops}")
+        child = StateNode(space_matrix=space_matrix, parent=self, path_operation=path_operation)
+        self.__children.__setitem__(path_operation, child)
+
+    def __find_element(self, space_matrix: List[List[int]], element: int) -> List[int]:
+        location: List[int] = [-1, -1]
+        for row_i, row in enumerate(space_matrix):
+            for col_i, elm in enumerate(row):
+                if elm == element:
+                    location = [row_i, col_i]
+        return location
+
+    def __swap(self, space_matrix: List[List[int]], el1_pos: List[int], el2_pos: List[int]) -> List[List[int]]:
+        if len(el1_pos) != 2 and len(el2_pos) != 2:
+            raise ValueError("Argument \"el1_pos\" and \"el2_pos\" must have exactly two integers.")
+        row_len = len(space_matrix)
+        col_len = len(space_matrix[0])
+        el1_x, el1_y = el1_pos
+        el2_x, el2_y = el2_pos
+        if(
+            not(
+                row_len, col_len >= el1_x, el1_y and
+                0, 0 <= el1_x, el1_y and
+                row_len, col_len >= el2_x, el2_y and
+                0, 0 <= el2_x, el2_y
+            )
+        ):
+            return space_matrix
+        temp = space_matrix[el1_x][el1_y]
+        space_matrix[el1_x][el1_y] = space_matrix[el2_x][el2_y]
+        space_matrix[el2_x][el2_y] = temp
+        return space_matrix
