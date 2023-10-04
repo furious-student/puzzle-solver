e12f8cf7b478a6b2232c56d639e7228f292c11d7
Martin Sivak <xsivakm1@stuba.sk>
2023-10-04 14:56:45 +0200

    Implement puzzle_solver class and its functionality to determine if the puzzle is solvable

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from state_node import StateNode
 from state_tree import StateTree
+from puzzle_solver import PuzzleSolver
 
 
 def main():
@@ -14,15 +15,34 @@ def main():
         [6, 7, 8]
     ]
     space_matrix3 = [
-        [8, 2, 3],
-        [4, 5, 1],
+        [8, 2, 5],
+        [4, 3, 1],
         [6, 7, 0]
+    ]
+
+    space_matrix_15_v1 = [
+        [1, 2, 3, 4],
+        [5, 6, 7, 8],
+        [9, 10, 11, 12],
+        [13, 14, 15, 0]
+    ]
+
+    space_matrix_15_v2 = [
+        [1, 2, 3, 4],
+        [5, 6, 7, 8],
+        [9, 10, 11, 12],
+        [13, 15, 14, 0]
     ]
     root = StateNode(space_matrix=space_matrix)
     root.create_children()
     state_tree = StateTree(root)
 
-    #state_tree.compute_h1(root.get_children(), space_matrix3)
+    # puzzle_solver = PuzzleSolver(space_matrix_15_v1, space_matrix_15_v2)
+    # puzzle_solver = PuzzleSolver(space_matrix, space_matrix2)
+    puzzle_solver = PuzzleSolver(space_matrix, space_matrix3)
+    print(puzzle_solver.is_solvable())
+
+    # state_tree.compute_h1(root.get_children(), space_matrix3)
     state_tree.compute_h2(root.get_children(), space_matrix3)
 
     for child in root.get_children().values():
@@ -33,4 +53,3 @@ def main():
 
 if __name__ == '__main__':
     main()
-
---
+++ puzzle_solver.py
@@ -0,0 +1,40 @@
+from state_tree import StateTree
+from state_node import find_element
+from typing import List
+
+
+class PuzzleSolver:
+    __initial_state: List[List[int]]
+    __final_state: List[List[int]]
+    __solution_path: List[str]
+
+    def __init__(self, initial_state: List[List[int]], final_state: List[List[int]]):
+        self.__initial_state = initial_state
+        self.__final_state = final_state
+
+    def is_solvable(self) -> bool:
+        def __flatten(list_2d: List[List[int]]) -> List[int]:
+            return [element for row in list_2d for element in row]
+
+        def __count_state_n(lst: List[int]) -> int:
+            def __count_elem_n(elem: int) -> int:
+                elem_n: int = 0
+                for el in lst:
+                    if el == elem or elem == 0:
+                        return elem_n
+                    if el == 0:
+                        continue
+                    if el > elem:
+                        elem_n += 1
+                return elem_n
+            state_n: int = 0
+            for e in lst:
+                state_n += __count_elem_n(e)
+            return state_n
+
+        flt_initial: List[int] = __flatten(self.__initial_state)
+        flt_final: List[int] = __flatten(self.__final_state)
+        initial_n: int = find_element(self.__initial_state, 0)[1] + __count_state_n(flt_initial) + 1
+        final_n: int = find_element(self.__final_state, 0)[1] + __count_state_n(flt_final) + 1
+        print("initial_n:", initial_n, "final_n", final_n)
+        return initial_n % 2 == final_n % 2
